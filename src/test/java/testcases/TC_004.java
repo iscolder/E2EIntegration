5da6dc3ec5ed8dde47aba821d8b415e91c821a95
kemelyanov <kemelyanov@evelopers.com>
2018-01-23 03:36:37 +0300

    added new test cases

---
+++ src/test/java/testcases/TC_004.java
@@ -0,0 +1,37 @@
+package testcases;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TC_004 {
+
+
+    private WebDriver webDriver;
+
+    @BeforeMethod
+    public void before() {
+        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+        webDriver = new FirefoxDriver();
+    }
+
+    @Test
+    public void testCase() {
+        webDriver.get("http://www.facebook.com");
+        webDriver.findElement(By.id("email")).sendKeys("Hello");
+        webDriver.findElement(By.id("pass")).sendKeys("Hello");
+    }
+
+
+    @AfterMethod
+    public void after() {
+        webDriver.quit();
+    }
+
+
+
+}
