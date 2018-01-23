4f7cdfe023538672dfcad6c84bfa8100101a47a9
kemelyanov <kemelyanov@evelopers.com>
2018-01-24 02:57:57 +0300

    added new test cases

---
+++ src/test/java/testcases/TC_006.java
@@ -0,0 +1,27 @@
+package testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TC_006 {
+
+
+    @BeforeMethod
+    public void before() {
+
+    }
+
+    @Test
+    public void testCase() {
+
+    }
+
+
+    @AfterMethod
+    public void after() {
+
+    }
+
+
+}
