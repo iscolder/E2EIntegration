3cfb3659e3902a983eef3d616b003a1d174292d1
kemelyanov <kemelyanov@evelopers.com>
2018-01-24 03:12:43 +0300

    added gitignore, modified test

--- src/test/java/testcases/TC_006.java
+++ src/test/java/testcases/TC_006.java
@@ -1,11 +1,13 @@
 package testcases;
 
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TC_006 {
-
 
     @BeforeMethod
     public void before() {
@@ -14,9 +16,10 @@ public class TC_006 {
 
     @Test
     public void testCase() {
-
+        Response response =  RestAssured.get("http://services.groupkt.com/country/get/iso2code/IN");
+        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertEquals(response.getContentType(), "application/json;charset=UTF-8");
     }
-
 
     @AfterMethod
     public void after() {
